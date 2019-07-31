084358ac5a8b343267bdb0ba22e592136f6e24d4
asapz <asapz@naver.com>
2019-07-31 14:41:57 +0900

    AuthContext part One

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from "apollo-boost";
@@ -11,12 +11,13 @@ import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import options from './assets/apollo';
 import styles from './styles';
+import NavController from './components/NavController';
+import { AuthProvider } from './AuthContext';
 
 export default function App() {
   // useState(false) = 기본값 로드 X
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(null);
   
   const preLoad = async() => {
     // Font 불러오기
@@ -39,13 +40,6 @@ export default function App() {
         cache,
         ...options
       });
-      // login여부 값 지정
-      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
-      if(isLoggedIn === null || isLoggedIn === false){
-        setIsLoggedIn(false);
-      }else{
-        setIsLoggedIn(true);
-      }
       setLoaded(true);
       setClient(client);
     }catch(e){
@@ -61,39 +55,13 @@ export default function App() {
     preLoad();
   }, []);
 
-  const logUserIn = async () => {
-    try {
-      //AsyncStorage는 값을 String으로 밖에 읽지 못한다.
-      await AsyncStorage.setItem("isLoggedIn", "true");
-      setIsLoggedIn(true);
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
-  const logUserOut = async () => {
-    try {
-      await AsyncStorage.setItem("isLoggedIn", "false");
-      setIsLoggedIn(false);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return loaded && client && !isLoggedIn !==null? (
-    <ApolloProvider client = {client}>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
       <ThemeProvider theme={styles}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          {isLoggedIn === true ? (
-            <TouchableOpacity onPress={logUserOut}>
-              <Text>Log Out</Text>
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={logUserIn}>
-              <Text>Log In</Text>
-            </TouchableOpacity>
-          )}
-        </View>
+        <AuthProvider>
+          <NavController />
+        </AuthProvider>
       </ThemeProvider>
     </ApolloProvider>) : (
     <AppLoading/>
